17513dbcc0197e43ae1fc110b0455aff46a63b09
minux <minux.ma@gmail.com>
2015-01-09 17:28:34 -0500

    hardcode key to 0, 1, 2, 3, ..., 0xf.
    Signed-off-by: minux <minux.ma@gmail.com>

--- siphash24.c
+++ siphash24.c
@@ -1,16 +1,3 @@
-/*
-   SipHash reference C implementation
-
-   Copyright (c) 2012-2014 Jean-Philippe Aumasson <jeanphilippe.aumasson@gmail.com>
-   Copyright (c) 2012-2014 Daniel J. Bernstein <djb@cr.yp.to>
-
-   To the extent possible under law, the author(s) have dedicated all copyright
-   and related and neighboring rights to this software to the public domain
-   worldwide. This software is distributed without any warranty.
-
-   You should have received a copy of the CC0 Public Domain Dedication along with
-   this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
- */
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
@@ -47,32 +34,15 @@
     v2 += v1; v1=ROTL(v1,17); v1 ^= v2; v2=ROTL(v2,32); \
   } while(0)
 
-#ifdef DEBUG                                                    
-#define TRACE                                                       \
-    do {                                                            \
-    printf( "(%3d) v0 %08x %08x\n",                                 \
-        ( int )inlen, ( uint32_t )( v0 >> 32 ), ( uint32_t )v0 );   \
-    printf( "(%3d) v1 %08x %08x\n",                                 \
-        ( int )inlen, ( uint32_t )( v1 >> 32 ), ( uint32_t )v1 );   \
-    printf( "(%3d) v2 %08x %08x\n",                                 \
-        ( int )inlen, ( uint32_t )( v2 >> 32 ), ( uint32_t )v2 );   \
-    printf( "(%3d) v3 %08x %08x\n",                                 \
-        ( int )inlen, ( uint32_t )( v3 >> 32 ), ( uint32_t )v3 );   \
-    } while(0)
-#else
-#define TRACE
-#endif
-
-int  siphash( uint8_t *out, const uint8_t *in, uint64_t inlen, const uint8_t *k )
-{
+int  siphash( uint8_t *out, const uint8_t *in, uint64_t inlen, const uint8_t *k ) {
   /* "somepseudorandomlygeneratedbytes" */
   uint64_t v0 = 0x736f6d6570736575ULL;
   uint64_t v1 = 0x646f72616e646f6dULL;
   uint64_t v2 = 0x6c7967656e657261ULL;
   uint64_t v3 = 0x7465646279746573ULL;
   uint64_t b;
-  uint64_t k0 = U8TO64_LE( k );
-  uint64_t k1 = U8TO64_LE( k + 8 );
+  uint64_t k0 = 0x0706050403020100ULL; //U8TO64_LE( k );
+  uint64_t k1 = 0x0F0E0D0C0B0A0908ULL; //U8TO64_LE( k + 8 );
   uint64_t m;
   int i;
   const uint8_t *end = in + inlen - ( inlen % sizeof( uint64_t ) );
@@ -83,23 +53,14 @@ int  siphash( uint8_t *out, const uint8_t *in, uint64_t inlen, const uint8_t *k 
   v1 ^= k1;
   v0 ^= k0;
 
-#ifdef DOUBLE
-  v1 ^= 0xee;
-#endif
-
-  for ( ; in != end; in += 8 )
-  {
+  for ( ; in != end; in += 8 ) {
     m = U8TO64_LE( in );
     v3 ^= m;
-
-    TRACE;
     for( i=0; i<cROUNDS; ++i ) SIPROUND;
-
     v0 ^= m;
   }
 
-  switch( left )
-  {
+  switch( left ) {
   case 7: b |= ( ( uint64_t )in[ 6] )  << 48;
   case 6: b |= ( ( uint64_t )in[ 5] )  << 40;
   case 5: b |= ( ( uint64_t )in[ 4] )  << 32;
@@ -110,36 +71,18 @@ int  siphash( uint8_t *out, const uint8_t *in, uint64_t inlen, const uint8_t *k 
   case 0: break;
   }
 
-
   v3 ^= b;
 
-  TRACE;
   for( i=0; i<cROUNDS; ++i ) SIPROUND;
 
   v0 ^= b;
 
-#ifndef DOUBLE
   v2 ^= 0xff;
-#else
-  v2 ^= 0xee;
-#endif
 
-  TRACE;
   for( i=0; i<dROUNDS; ++i ) SIPROUND;
 
   b = v0 ^ v1 ^ v2  ^ v3;
   U64TO8_LE( out, b );
 
-#ifdef DOUBLE
-  v1 ^= 0xdd;
-
-  TRACE;
-  for( i=0; i<dROUNDS; ++i ) SIPROUND;
-
-  b = v0 ^ v1 ^ v2  ^ v3;
-  U64TO8_LE( out+8, b );
-#endif
-
   return 0;
 }
-
