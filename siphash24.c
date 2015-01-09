41be2d1e9c65fcc04dcb4e1c5647025833b4bb4c
minux <minux.ma@gmail.com>
2015-01-09 17:28:47 -0500

    make it slower by removing the duff's device with a loop
    Signed-off-by: minux <minux.ma@gmail.com>

--- siphash24.c
+++ siphash24.c
@@ -1,6 +1,4 @@
 #include <stdint.h>
-#include <stdio.h>
-#include <string.h>
 
 /* default: SipHash-2-4 */
 #define cROUNDS 2
@@ -46,7 +44,7 @@ int  siphash( uint8_t *out, const uint8_t *in, uint64_t inlen, const uint8_t *k 
   uint64_t m;
   int i;
   const uint8_t *end = in + inlen - ( inlen % sizeof( uint64_t ) );
-  const int left = inlen & 7;
+  int left = inlen & 7;
   b = ( ( uint64_t )inlen ) << 56;
   v3 ^= k1;
   v2 ^= k0;
@@ -60,16 +58,8 @@ int  siphash( uint8_t *out, const uint8_t *in, uint64_t inlen, const uint8_t *k 
     v0 ^= m;
   }
 
-  switch( left ) {
-  case 7: b |= ( ( uint64_t )in[ 6] )  << 48;
-  case 6: b |= ( ( uint64_t )in[ 5] )  << 40;
-  case 5: b |= ( ( uint64_t )in[ 4] )  << 32;
-  case 4: b |= ( ( uint64_t )in[ 3] )  << 24;
-  case 3: b |= ( ( uint64_t )in[ 2] )  << 16;
-  case 2: b |= ( ( uint64_t )in[ 1] )  <<  8;
-  case 1: b |= ( ( uint64_t )in[ 0] ); break;
-  case 0: break;
-  }
+  for (; left; left--)
+	  b |= ((uint64_t)in[left-1]) << (8*left-8);
 
   v3 ^= b;
 
