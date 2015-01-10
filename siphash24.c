7c5a5a665efecd4873ff57403f19a30a9357d1b0
minux <minux.ma@gmail.com>
2015-01-10 06:01:00 -0500

    avoid use of C++ keyword xor.
    Signed-off-by: minux <minux.ma@gmail.com>

--- siphash24.c
+++ siphash24.c
@@ -5,7 +5,7 @@ typedef struct { /* assume unsigned long is at least 32-bit */
   unsigned long lo;
 } _uint64;
 
-static inline void rotl(_uint64 *v, int bits) { /* bits must <=32 */
+static inline void _rotl(_uint64 *v, int bits) { /* bits must <=32 */
   unsigned long tmp = v->hi;
   if (bits == 32) {
     v->hi = v->lo;
@@ -16,22 +16,22 @@ static inline void rotl(_uint64 *v, int bits) { /* bits must <=32 */
   }
 }
 
-static inline void xor(_uint64 *dst, _uint64 *src) {
+static inline void _xor(_uint64 *dst, _uint64 *src) {
   dst->hi ^= src->hi;
   dst->lo ^= src->lo;
 }
 
-static inline void add(_uint64 *dst, _uint64 *src) {
+static inline void _add(_uint64 *dst, _uint64 *src) {
   dst->lo += src->lo;
   dst->hi += src->hi + ((dst->lo & 0xfffffffful) < (src->lo&0xfffffffful) ? 1:0);
 }
 
 #define SIPROUND                                        \
   do {                                                  \
-    add(&v0, &v1); rotl(&v1, 13); xor(&v1, &v0); rotl(&v0, 32); \
-    add(&v2, &v3); rotl(&v3, 16); xor(&v3, &v2); \
-    add(&v0, &v3); rotl(&v3, 21); xor(&v3, &v0); \
-    add(&v2, &v1); rotl(&v1, 17); xor(&v1, &v2); rotl(&v2, 32); \
+    _add(&v0, &v1); _rotl(&v1, 13); _xor(&v1, &v0); _rotl(&v0, 32); \
+    _add(&v2, &v3); _rotl(&v3, 16); _xor(&v3, &v2); \
+    _add(&v0, &v3); _rotl(&v3, 21); _xor(&v3, &v0); \
+    _add(&v2, &v1); _rotl(&v1, 17); _xor(&v1, &v2); _rotl(&v2, 32); \
   } while(0)
 
 int siphash(unsigned char *out, const unsigned char *in, unsigned long inlen, const unsigned char *k /*unused*/) {
@@ -48,14 +48,14 @@ int siphash(unsigned char *out, const unsigned char *in, unsigned long inlen, co
   const int cROUNDS = 2, dROUNDS = 4;
   const unsigned char *end = in + inlen - (inlen % 8);
   int left = inlen & 7;
-  xor(&v3, &k1); xor(&v2, &k0); xor(&v1, &k1); xor(&v0, &k0);
+  _xor(&v3, &k1); _xor(&v2, &k0); _xor(&v1, &k1); _xor(&v0, &k0);
   for (; in != end; in += 8) {
     b.hi = 0; b.lo = 0;
     for (i = 0; i < 4; i++) b.lo |= ((unsigned long)in[i]) << (8*i);
     for (i = 0; i < 4; i++) b.hi |= ((unsigned long)in[i+4]) << (8*i);
-    xor(&v3, &b);
+    _xor(&v3, &b);
     for (i = 0; i < cROUNDS; i++) SIPROUND;
-    xor(&v0, &b);
+    _xor(&v0, &b);
   }
   b.hi = (inlen & 0xff)<<24; b.lo = 0;
   for (; left; left--)
@@ -63,12 +63,12 @@ int siphash(unsigned char *out, const unsigned char *in, unsigned long inlen, co
       b.hi |= ((unsigned long)in[left-1]) << (8*left-8-32);
     else
       b.lo |= ((unsigned long)in[left-1]) << (8*left-8);
-  xor(&v3, &b);
+  _xor(&v3, &b);
   for(i=0; i<cROUNDS; i++) SIPROUND;
-  xor(&v0, &b); v2.lo ^= 0xff;
+  _xor(&v0, &b); v2.lo ^= 0xff;
   for(i=0; i<dROUNDS; i++) SIPROUND;
   b.lo = 0; b.hi = 0;
-  xor(&b, &v0); xor(&b, &v1); xor(&b, &v2); xor(&b, &v3);
+  _xor(&b, &v0); _xor(&b, &v1); _xor(&b, &v2); _xor(&b, &v3);
   for (i = 0; i < 8; i++) {
     unsigned long *t = i > 3 ? &b.hi : &b.lo;
     out[i] = *t;
